22d3908a9d3fa74db8b7280befbe132b584b2bf1
aronnuyenn <aronnguyen96@gmail.com>
2022-05-03 12:50:50 +0700

    change message

---
+++ test.js
@@ -0,0 +1,47 @@
+function a() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("aa");
+      //   resolve(1);
+      reject(1);
+    }, 500);
+  });
+}
+
+async function b() {
+  try {
+    const rs = await a();
+    console.log(rs);
+    console.log("aaa");
+  } catch (error) {
+    console.log("erer");
+    console.log(error);
+    
+  }
+  //   a()
+  //     .then((rs) => {
+  //       console.log(rs);
+  //     })
+  //     .catch((err) => {
+  //       console.log('ee',err);
+  //     })
+  //     .finally(() => {
+  //       console.log(11);
+  //     });
+  //     console.log("bb");
+  //     console.log("bb");
+  //       a()
+  //         .then((rs) => {
+  //           console.log(rs);
+  //         })
+  //         .catch((err) => {
+  //           console.log("ee", err);
+  //         })
+  //         .finally(() => {
+  //           console.log(11);
+  //         });
+  //     console.log("bb");
+  //     console.log("bb");
+}
+
+b();
